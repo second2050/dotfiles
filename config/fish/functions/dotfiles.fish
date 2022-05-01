9428accc82dda463bd4cc4920d49b95255e80aa9
second2050 <git@second2050.me>
2022-05-01 14:46:05 +0200

    add dotfiles manager script

---
+++ config/fish/functions/dotfiles.fish
@@ -0,0 +1,65 @@
+function dotfiles --description="manage the dotfile repo"
+	set DOTFILES_DIR "$HOME/.dotfiles"
+	switch $argv[1]
+		case 'add'
+			if test (pwd) != "$HOME/.config"
+				echo "Not in correct directory ($HOME/.config), you are in" (pwd)
+				return 1 # exit if not in ~/.config
+			end
+			if set -q argv[2]
+				echo "Adding $argv[2] to dotfiles repository"
+				mv $argv[2] "$DOTFILES_DIR"/config/"$argv[2]"
+				ln -sfv "$DOTFILES_DIR"/config/"$argv[2]" $argv[2]
+			else
+				echo "File/Directory name needed."
+				return 2
+			end
+
+		case 'remove'
+			if test (pwd) != "$HOME/.config"
+				echo "Not in correct directory ($HOME/.config), you are in" (pwd)
+				return 1 # exit if not in ~/.config
+			end
+			if set -q argv[2]
+				if test -e "$DOTFILES_DIR"/config/"$argv[2]"
+					echo "Removing $argv[2] from dotfiles repository"
+					rm $argv[2]
+					mv "$DOTFILES_DIR"/config/"$argv[2]" $argv[2]
+				else
+					echo "$argv[2] not found in dotfiles repository"
+					return 3
+				end
+			else
+				echo "File/Directory name needed."
+				return 2
+			end
+
+		case 'deploy'
+			echo "Redeploying dotfiles"
+			cd $DOTFILES_DIR
+			$DOTFILES_DIR/setup_symlinks.sh
+			return $status
+
+		case 'commit'
+			cd $DOTFILES_DIR
+			git add .
+			if set -q argv[2]
+				git commit -m "$argv[2]"	
+			else
+				git commit -m "Update from $(date +"%Y-%m-%d %T")"
+			end
+
+		case 'pull'
+			cd $DOTFILES_DIR
+			git pull
+			dotfiles deploy
+
+		case 'push'
+			cd $DOTFILES_DIR
+			git push
+
+		case '*'
+			echo "Available subcommands: add, remove, deploy, commit, pull, push"
+
+	end
+end
--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# check if we are executed in the correct directory
+if [[ ! -f ./setup_symlinks.sh ]]; then
+	echo "PANIC: I am not executed in the correct directory!!!"
+	exit -1
+fi
+
 # ~/.config
 ln -sfv "$(pwd)"/config/* -t "$HOME"/.config/
 
 # ~
 (
-	cd ./home
+	cd ./home || return
 	for file in *; do
 		ln -sfv "$(pwd)/$file" "$HOME"/."$file"
 	done

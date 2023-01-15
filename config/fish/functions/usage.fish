4f31ed825f6291ed475e42f901e96d0deaf64f85
second2050 <git@second2050.me>
2023-01-15 01:22:18 +0100

    fish: add usage script to quickly get size of folder

---
+++ config/fish/functions/usage.fish
@@ -0,0 +1,9 @@
+function usage --wraps="du --summarize --human-readable" --description 'Shows size of files and directories'
+    set --local cmd du --summarize --human-readable
+
+    if count $argv > /dev/null
+        $cmd $argv
+    else
+        $cmd (pwd)
+    end
+end

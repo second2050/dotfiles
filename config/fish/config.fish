f46f53d36e083452104f9874c2289041537dab56
second2050 <git@second2050.me>
2023-03-31 22:10:10 +0200

    fish: update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,18 @@
+# start ZelliJ if started as login-shell
+if status is-login
+    if set -q ZELLIJ || set -q TMUX # do not nest multiplexers
+    else
+        set ZJ_SESSIONS (zellij list-sessions 2> /dev/null | string split "\n")
+        set NO_SESSIONS (echo $ZJ_SESSIONS | wc -w)
+
+        if test $NO_SESSIONS -ge 2
+            exec zellij attach (echo $ZJ_SESSIONS | string split " " | fzf)
+        else
+            exec zellij attach --create
+        end
+    end
+end
+
 # Override fish config
 #set -g fish_prompt_pwd_dir_length 80
 
@@ -13,3 +28,5 @@ export EDITOR=/usr/bin/vim
 
 # Load Starship Prompt
 starship init fish | source
+enable_transience
+
---
+++ config/fish/functions/starship_transient_prompt_func.fish
@@ -0,0 +1,5 @@
+function starship_transient_prompt_func
+printf "\n"
+#starship module time
+starship module character
+end

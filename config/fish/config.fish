eb439b39f4489684a9cb53188398f16fde3e6793
second2050 <git@second2050.me>
2023-07-20 17:43:10 +0200

    fish: show systeminfo via neofetch if asciinema is recording

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,3 +15,10 @@ export EDITOR=/usr/bin/vim
 starship init fish | source
 enable_transience
 
+# show short system overview when recording with asciinema
+if set -q ASCIINEMA_REC && test "$ASCIINEMA_REC" -eq 1
+    neofetch --no_config --ascii_distro arch_small \
+        --disable term theme icons cpu gpu memory packages wm de \
+                  wm_theme uptime model resolution cols
+end
+

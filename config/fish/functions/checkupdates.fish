4f22228d577ef57d5fa83e23f5f7d79056ba3815
second2050 <git@second2050.me>
2023-07-30 14:38:55 +0200

    fish: update `checkupdates` function

--- config/fish/functions/checkupdates.fish
+++ config/fish/functions/checkupdates.fish
@@ -1,18 +1,20 @@
 function checkupdates
-    echo Checking for updates...
+    echo -e "\033[1m\033[34m::\033[39m Checking for updates..."
     command checkupdates > /tmp/chkupd_tmprslt
-    if test $status -eq 0
-        begin
-            echo "Update Information"
-            echo "=================="
-            echo " ==> $(cat /tmp/chkupd_tmprslt | wc -l) updates pending..."
-            echo ""
-            echo " ==> Package list:"
-            cat /tmp/chkupd_tmprslt
-        end | less --quit-if-one-screen
-    else if test $status -eq 2
-        echo System is up to date!
+    set chkstatus $status
+
+    # check if checkupdates ran correctly 
+    if test $chkstatus -eq 2 # statuscode `2` is used by checkupdates if the system is up to date
+        echo -e "\033[1m\033[34m::\033[39m System is up to date!"
         return 0
+    else if test $chkstatus -ne 0 # catch all for everything else
+        echo -e "\033[1m\033[34m::\033[39m 'checkupdates' ran into an issue. \033[31mError $chkstatus"
     end
+
+    # output pending updates list
+    begin
+        echo -e "\033[1m\033[34m::\033[39m $(cat /tmp/chkupd_tmprslt | wc -l) updates pending..."
+        cat /tmp/chkupd_tmprslt | awk '{ print "\033[1m" $1 "\033[0m \033[90m" $2 "\033[0m \033[90m" "\033[1m→" "\033[0m \033[90m" $4 "\033[0m" }'
+    end | less --quit-if-one-screen --raw-control-chars
 end
 

212aa3cbd63d7d36590aee8e19c898782dc2ad43
second2050 <git@second2050.me>
2023-11-30 09:41:28 +0100

    fish: cleanup config and introduce if-guard for starship

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,3 @@
-# Override fish config
-#set -g fish_prompt_pwd_dir_length 80
-
 # switch to zellij if connected via ssh and interactive
 if status is-interactive && set -q SSH_CLIENT && which zellij
     set --erase SSH_CLIENT
@@ -9,22 +6,24 @@ if status is-interactive && set -q SSH_CLIENT && which zellij
     exec zellij attach --create
 end
 
-# Set SHELL to fish
-export SHELL=/usr/bin/fish
-
 # Keybindings (Konsole/Yakuake)
 bind \ch backward-kill-word # Kill left Word with CTRL+BACKSPACE
 bind \e\[3\;5~ kill-word    # Kill right Word with CTRL+DELETE
 
 # Variables
-export EDITOR=/usr/bin/vim
+export SHELL=(which fish)
+export EDITOR=(which vim 2> /dev/null)
 
-# Load Starship Prompt
-starship init fish | source
-enable_transience
+# Load Starship Prompt if available
+if which starship &> /dev/null
+    starship init fish | source
+    enable_transience
+else
+    echo ":: [starship] not available, falling back to simple prompt"
+end
 
-# show short system overview when recording with asciinema
-if set -q ASCIINEMA_REC && test "$ASCIINEMA_REC" -eq 1
+# show short system overview when recording with asciinema (if neofetch is available)
+if set -q ASCIINEMA_REC && test "$ASCIINEMA_REC" -eq 1 && which neofetch &> /dev/null
     neofetch --no_config --ascii_distro arch_small \
         --disable term theme icons cpu gpu memory packages wm de \
                   wm_theme uptime model resolution cols

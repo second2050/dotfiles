eddb792c63a8769a05397bce48ee0490ad831bb8
second2050 <git@second2050.me>
2024-04-19 11:33:33 +0200

    fish: set XDG envvars if they are unset

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -21,6 +21,10 @@ bind \ei __sekii_show_git_status
 # Variables
 set --export SHELL (command --search fish)
 set --export EDITOR (command --search vim)
+if not set -q XDG_CONFIG_HOME; set --export XDG_CONFIG_HOME "$HOME/.config"; end
+if not set -q XDG_CACHE_HOME; set --export XDG_CACHE_HOME "$HOME/.cache"; end
+if not set -q XDG_DATA_HOME; set --export XDG_DATA_HOME "$HOME/.local/share"; end
+if not set -q XDG_STATE_HOME; set --export XDG_STATE_HOME "$HOME/.local/state"; end
 set FISH_FUNCTION_DIR "$XDG_CONFIG_HOME/fish/functions"
 
 # Load Starship Prompt if available

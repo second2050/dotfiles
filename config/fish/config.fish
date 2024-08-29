04d16efa52462c44a5744a63afcf16f1439a3008
Dennis Neuhaus <d.neuhaus@factorycampus.biz>
2024-08-29 19:24:29 +0200

    fish: update EDITOR variable

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -24,7 +24,7 @@ bind \ei __sekii_show_git_status
 
 # Variables
 set --export SHELL (command --search fish)
-set --export EDITOR (command --search vim)
+set --export EDITOR (command --search nvim || command --search vim || command --search vi)
 if not set -q XDG_CONFIG_HOME; set --export XDG_CONFIG_HOME "$HOME/.config"; end
 if not set -q XDG_CACHE_HOME; set --export XDG_CACHE_HOME "$HOME/.cache"; end
 if not set -q XDG_DATA_HOME; set --export XDG_DATA_HOME "$HOME/.local/share"; end

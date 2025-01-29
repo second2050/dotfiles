3d642edb4e2436b14d8fa0e13af94dada38e5354
second2050 <git@second2050.me>
2025-01-29 14:12:00 +0100

    fish: update functions

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -12,5 +12,10 @@ function fish_right_prompt
     # show mommy in other cases if available
     else if command -q mommy && ! set -q MOMMY_SLEEP && ! set -q ASCIINEMA_REC
         mommy -1 -s $cmd_status 2> /dev/null
+    # show cute kaomojis based on status if mommy is n/a or asleep
+    else if test "$cmd_status" -eq 0
+        echo -e "\e[90m(>‿◕)~♥\e[0m"
+    else
+        echo -e "\e[90m(╯°□°)╯︵ ┻━┻\e[0m"
     end
 end

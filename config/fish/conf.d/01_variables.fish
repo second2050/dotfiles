88dec652bc7f2b7a1c8e805cac9db3e0b1c5e938
second2050 <git@second2050.me>
2025-03-26 20:20:44 +0100

    fish: set envvars for less

--- config/fish/conf.d/01_variables.fish
+++ config/fish/conf.d/01_variables.fish
@@ -7,3 +7,14 @@ if not set -q XDG_DATA_HOME; set --export XDG_DATA_HOME "$HOME/.local/share"; en
 if not set -q XDG_STATE_HOME; set --export XDG_STATE_HOME "$HOME/.local/state"; end
 set FISH_FUNCTION_DIR "$__fish_config_dir/functions"
 
+## Less options
+set --local less_promptM '?f%f .?n?m(%T %i of %m) ..?ltlines %lt-%lb?L/%L. :byte %bB?s/%s. .?e(END) ?x- Next\: %x.:?pB%pB\%..%t (press h for help or q to quit)'
+set --export LESS "-RSMPM$less_promptM\$ --mouse --file-size"
+set --export LESS_TERMCAP_mb (set_color -o red)
+set --export LESS_TERMCAP_md (set_color -o blue)
+set --export LESS_TERMCAP_me (printf "\e[0m")
+set --export LESS_TERMCAP_so (set_color green)
+set --export LESS_TERMCAP_se (printf "\e[0m")
+set --export LESS_TERMCAP_us (set_color -u magenta)
+set --export LESS_TERMCAP_ue (printf "\e[0m")
+if command --query bat; set --export LESSOPEN '|bat --color always %s'; end

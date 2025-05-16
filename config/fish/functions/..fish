51a05cfae2eea3cd22533cf6402757d9c7093d52
second2050 <git@second2050.me>
2025-05-16 12:07:39 +0200

    fish: update dot function

--- config/fish/functions/..fish
+++ config/fish/functions/..fish
@@ -3,7 +3,9 @@ if not command --query nix
 end
 
 function . --wraps="nix run" --description 'run applications ephimerally via nix run'
-    if echo "$argv[1]" | grep "#" || echo "$argv[1]" | grep ":"
+    if test "$argv[1]" = ""
+        echo -e "usage:\n → . hello\n → . nixpkgs#hello\n → . github:nix-community/plasma-manager"
+    else if echo "$argv[1]" | grep -e "#" -e ":" > /dev/null
         nix run "$argv[1]" -- $argv[2..]
     else
         nix run "nixpkgs#$argv[1]" -- $argv[2..]

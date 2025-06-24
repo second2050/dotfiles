e50aeed69fcfcc1528594c44748c0ab905797618
second2050 <git@second2050.me>
2025-06-24 20:39:46 +0200

    nvim: include nixd config

--- config/nvim/lua/second2050/lsp/lazy-lsp.lua
+++ config/nvim/lua/second2050/lsp/lazy-lsp.lua
@@ -3,12 +3,22 @@ if not status_ok then
     return
 end
 
+local hostname_fd = io.popen("hostname")
+local hostname = "ringo"
+if hostname_fd ~= nil then
+    hostname = hostname_fd:read()
+    hostname_fd:close()
+end
+
 require("second2050.lsp.handlers").setup()
 lazy_lsp.setup {
     prefer_local = true,
     preferred_servers = {
         html = { "html", "ltex" },
         markdown = { "marksman", "ltex" },
+        nix = { "nixd" },
+        python = { "basedpyright" },
+        cpp = { "clangd" },
     },
     default_config = {
         on_attach = require("second2050.lsp.handlers").on_attach,
@@ -23,6 +33,23 @@ lazy_lsp.setup {
                     diagnostics = {
                         -- Get the language server to recognize the `vim` global
                         globals = { "vim" },
+                    },
+                },
+            },
+        },
+        ltex = {
+            settings = {
+                ltex = {
+                    language = "de-DE",
+                },
+            },
+        },
+        nixd = {
+            settings = {
+                nixd = {
+                    options = {
+                        nixos = { expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations." .. hostname .. ".options" },
+                        home_manager = { expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations." .. hostname .. ".options.home-manager.users.type.getSubOptions []" },
                     },
                 },
             },
